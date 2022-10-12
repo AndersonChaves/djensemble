ddba39120e313bb653e57207e9f17793c139d6b8
AndersonChaves <andersonchaves.net@gmail.com>
2022-10-12 16:53:50 -0300

    Initial Commit

---
+++ core/categorization.py
@@ -0,0 +1,309 @@
+from .series_generator import SeriesGenerator
+import numpy as np
+from gldpy import GLD
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from math import floor, ceil
+from itertools import cycle
+from .dataset_manager import DatasetManager
+
+def print_array(array_list):
+    from matplotlib import pyplot as plt
+
+    if isinstance(array_list, list):
+        n = len(array_list)
+        f, axis = plt.subplots(1, n)
+        title_list = ["Clustering", "Tiling"]
+        for array, i, title in zip(array_list, range(n), title_list):
+            axis[i].set_title(title)
+            axis[i].imshow(array)
+    else:
+        plt.imshow(array_list, interpolation='nearest')
+    plt.show()
+
+def create_tiles_dictionary_mock(shape):
+    _, lat, long = shape
+    d = {}
+    d[0] = {"lat": (0,                  floor(lat / 2)), "long": (0, floor(long / 2))}
+    d[1] = {"lat": (floor(lat / 2)    , ceil(lat / 2)), "long": (0, floor(long / 2))}
+    d[2] = {"lat": (0,                  floor(lat / 2)), "long": (floor(long / 2), ceil(long / 2))}
+    d[3] = {"lat": (floor(lat / 2)    , ceil(lat / 2)), "long": (floor(long / 2), ceil(long / 2))}
+    return d
+
+def expand_tile(tiling: np.array, clustering: np.array, start: tuple, tile_id, max_impurity = 2):
+    impurity = 0
+
+    lat, long = clustering.shape
+    tile_cluster = clustering[start]
+    cursor = end = start
+    tiling[cursor] = tile_id
+
+    dir_remaining = 4
+    directions = cycle([(1, 0), (0, 1), (-1, 0), (0, -1)])
+    skip_list = []
+    for dir_step in directions:
+        if len(skip_list) == 4:
+            break
+        if dir_step in skip_list:
+            continue
+        updates = True
+        current_impurity = impurity
+        #---EXPAND RIGHT---------------------------
+        if dir_step == (1, 0) and end[1]+1 < long: #todo: Create Tile Objetct for Expansion
+            end = end[0], end[1]+1
+            x_start, y_start = start[0], end[1]
+            x_end, y_end = end
+            for i in range(x_start, x_end+1):
+                cursor = i, y_start
+                if tiling[cursor] != -1:
+                    end = end[0], end[1] - 1
+                    updates = False
+                    break
+                elif clustering[cursor] != tile_cluster:
+                    if current_impurity == max_impurity:
+                        end = end[0], end[1] - 1
+                        updates=False
+                        break
+                    else:
+                        current_impurity += 1
+            if updates:
+                for i in range(x_start, x_end + 1):
+                    cursor = i, y_start
+                    tiling[cursor] = tile_id
+        # ---EXPAND DOWN---------------------------
+        elif dir_step == (0, 1) and end[0]+1 < lat:
+            end = end[0]+1, end[1]
+            x_start, y_start = end[0], start[1]
+            x_end, y_end = end
+            for i in range(y_start, y_end + 1):
+                cursor = x_start, i
+                if tiling[cursor] != -1:
+                    updates = False
+                    end = end[0] - 1, end[1]
+                    break
+                if clustering[cursor] != tile_cluster:
+                    if current_impurity == max_impurity:
+                        updates=False
+                        end = end[0] - 1, end[1]
+                        break
+                    else:
+                        current_impurity += 1
+            if updates:
+                for i in range(y_start, y_end + 1):
+                    cursor = x_start, i
+                    tiling[cursor] = tile_id
+        # ---EXPAND LEFT---------------------------
+        elif dir_step == (-1, 0) and start[1]-1 >= 0:
+            start = start[0], start[1]-1
+            x_start, y_start = start
+            x_end, y_end = end[0], start[1]+1
+            for i in range(x_start, x_end + 1):
+                cursor = i, y_start
+                if tiling[cursor] != -1:
+                    start = start[0], start[1] + 1
+                    updates = False
+                    break
+                if clustering[cursor] != tile_cluster:
+                    if current_impurity == max_impurity:
+                        start = start[0], start[1]+1
+                        updates = False
+                        break
+                    else:
+                        current_impurity += 1
+            if updates:
+                for i in range(x_start, x_end + 1):
+                    cursor = i, y_start
+                    tiling[cursor] = tile_id
+
+        # ---EXPAND UP---------------------------
+        elif dir_step == (0, -1) and start[0]-1 >= 0:
+            start = start[0]-1, start[1]
+            x_start, y_start = start
+            x_end, y_end = start[0], end[1]
+            for i in range(y_start, y_end + 1):
+                cursor = x_start, i
+                if tiling[cursor] != -1:
+                    updates = False
+                    start = start[0] + 1, start[1]
+                    break
+                if clustering[cursor] != tile_cluster:
+                    if current_impurity == max_impurity:
+                        updates=False
+                        start = start[0] + 1, start[1]
+                        break
+                    else:
+                        current_impurity += 1
+            if updates:
+                for i in range(y_start, y_end + 1):
+                    cursor = x_start, i
+                    tiling[cursor] = tile_id
+        #----------------------------------------
+        else:
+            updates = False
+        if updates:
+            impurity = current_impurity
+        else:
+            skip_list.append(dir_step)
+            dir_remaining -= 1
+    return {"start": start, "end": end}
+
+def create_tiling(clustering: np.array):
+    shp = clustering.shape
+    lat, long = shp
+    tiling = np.full(shp, -1)
+    x = 0
+    tile_id = 1
+    tile_dict = {}
+    while x < lat:
+        y = 0
+        while y < long:
+            if tiling[x, y] == -1:
+                tile_dict[tile_id] = expand_tile(tiling, clustering, (x, y), tile_id)
+                tile_id += 1
+            y += 1
+        x += 1
+    tiling = [[0, 0, 0, 0, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1],
+              [0, 0, 0, 0, 1, 1, 1],
+              [3, 3, 3, 3, 3, 3, 3],
+              [3, 3, 3, 3, 3, 3, 3]]
+    tile_dict = {1: {'start': (5, 0), 'end': (6, 6)},
+                 2: {'start': (0, 0), 'end': (4, 3)},
+                 3: {'start': (0, 4), 'end': (4, 6)},}
+    return tiling, tile_dict
+
+def calculate_gld_list_from_dataset(target_dataset):
+    time, lat, long = target_dataset.shape
+    # print("Calculating GLD")
+    gld_list = np.empty((0, 4), float)
+    for i in range(lat):
+        print("Calculating GLDs for lat", i)
+        for j in range(long):
+            cut_start, cut_ending = 0, time
+
+            X, _ = SeriesGenerator().split_series_into_set_of_fixed_size_series(
+                target_dataset[cut_start:cut_ending, i, j], time, n_steps_out=0)
+            X = X.reshape((len(X[0])))
+            # gld = GLD('RS')
+            gld = GLD('FMKL')
+            # param_MM = gld.fit_MM(X, [0.5, 1], bins_hist=20, maxiter=1000, maxfun=1000)
+            indexes = np.array((range(len(X))))
+
+            while (True):
+                # guess = [random.randint(0, 2), random.randint(0, 2)]
+                guess = (1, 1)
+                try:
+                    param_MM = gld.fit_curve(indexes, X, initial_guess=guess, N_gen=1000,
+                                             optimization_phase=False, shift=True, disp_fit=False)
+                    gld_list = np.append(gld_list, np.reshape(param_MM[0], (1, 4)), axis=0)
+                    break
+                except:
+                    continue
+    print("GLDs calculated")
+    return gld_list
+
+def cluster_dataset(target_dataset):
+    gld_list = calculate_gld_list_from_dataset(target_dataset)
+
+    # Cluster using k-means into n clusters
+    number_of_clusters = 1
+    best_silhouete = -2
+    kmeans_best_clustering = [0 for _ in range(len(gld_list))]
+    for number_of_clusters in range(2, 5+1):
+        kmeans = KMeans(n_clusters=number_of_clusters, random_state=0)
+        kmeans_labels = kmeans.fit_predict(gld_list)
+        silhouette_avg = silhouette_score(gld_list, kmeans_labels)
+        if silhouette_avg > best_silhouete:
+            kmeans_best_clustering = kmeans_labels
+            best_silhouete = silhouette_avg
+    print("KMeans best clustering: ", kmeans_best_clustering)
+    print("KMeans best silhouete: ", best_silhouete)
+    return gld_list, kmeans_best_clustering
+
+def calculate_centroid(clustering, start, end):
+    tile_glds = clustering[start[0]:end[0]+1, start[1]:end[1]+1]
+    t_shp = tile_glds.shape
+    centroid_gld = [np.average(tile_glds[:, :, p]) for p in range(4)]
+
+    from sklearn.metrics import pairwise_distances_argmin_min
+    c, _ = pairwise_distances_argmin_min(np.reshape(centroid_gld, (1, -1)),
+                                     np.reshape(tile_glds, (t_shp[0] * t_shp[1], 4)))
+    print("Centroid: ", c)
+    centroid = c // t_shp[1], c % t_shp[1]
+    # Returns the centroid position relative to the tile
+    return centroid
+
+def categorize_dataset(target_dataset):
+    time, lat, long = target_dataset.shape
+    gld_list, kmeans_best_clustering = cluster_dataset(target_dataset)
+    clustering_reshaped = np.reshape(kmeans_best_clustering, newshape=(lat, long))
+    tiling, tile_dict = create_tiling(clustering_reshaped)
+    print_array([clustering_reshaped, tiling])
+
+    tile_dim_dict = {}
+    for tile_id, value in tile_dict.items():
+        tile_dim_dict[tile_id] = {}
+        start, end = value['start'], value['end']
+        tile_dim_dict[tile_id]["lat"] = (start[0], end[0])
+        tile_dim_dict[tile_id]["long"] = (start[1], end[1])
+        gld_list = np.reshape(gld_list, (lat, long, 4)) # Number of GLD parameters = 4
+        tile_dim_dict[tile_id]["centroid"] = calculate_centroid(gld_list,
+                                                                start, end)
+    return tile_dim_dict
+
+
+def test_1():
+    tiling = np.full((5, 5), -1)
+    clustering = np.array([[1, 1, 1, 1, 1],
+                           [1, 1, 1, 0, 0],
+                           [1, 1, 1, 1, 0],
+                           [1, 1, 1, 0, 0],
+                           [1, 1, 1, 0, 0]])
+    print(expand_tile(tiling, clustering, (0, 0), 1))
+
+def test_2():
+    tiling = np.full((5, 5), -1)
+    clustering = np.array([[1, 1, 1, 1, 1],
+                           [1, 1, 1, 0, 0],
+                           [1, 1, 1, 1, 0],
+                           [1, 1, 1, 0, 0],
+                           [1, 1, 1, 0, 0]])
+    print(expand_tile(tiling, clustering, (0, 1), 1))
+
+def test_3():
+    tiling = np.full((5, 5), -1)
+    clustering = np.array([[1, 1, 1, 1, 1],
+                           [1, 1, 1, 0, 0],
+                           [1, 1, 1, 1, 0],
+                           [1, 1, 1, 0, 0],
+                           [1, 0, 1, 0, 0]])
+    print(expand_tile(tiling, clustering, (1, 1), 1))
+
+def test_clustering():
+    dataset = np.full((10, 5, 5), 7)
+    clustering = cluster_dataset(dataset)
+    print_array(np.reshape(clustering, (5, 5)))
+
+
+def test_tiling():
+    clustering = np.array([[1, 1, 1, 1, 1],
+                           [1, 0, 1, 0, 0],
+                           [1, 1, 1, 1, 0],
+                           [1, 1, 1, 0, 0],
+                           [1, 0, 1, 0, 0]])
+    create_tiling(clustering)
+
+
+
+def test_categorization():
+    array = DatasetManager().synthetize_dataset(shape=(10, 10, 10))
+    print(categorize_dataset(array))
+
+
+if __name__ == "__main__":
+    test_categorization()
+    #print_array()
+    # test_clustering()
+    # print(gld)
---
+++ core/learner.py
@@ -0,0 +1,181 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from functools import reduce
+import dtw
+
+from .noise_generator import NoiseGenerator
+from .series_generator import SeriesGenerator
+from .simple_regressor import LinearRegressor
+from .convolutional_model_invoker import ConvolutionalModelInvoker
+from .categorization import calculate_centroid
+from .categorization import calculate_gld_list_from_dataset
+from .dataset import Dataset
+from abc import ABC, abstractmethod
+
+class Learner(ABC):
+    model = None
+    _is_temporal_model = False
+    temporal_length = 10
+
+    def __init__(self, model_directory, model_name,
+                 is_temporal_model=False):
+        self.model_directory = model_directory
+        self.model_name = model_name
+        self.number_of_training_samples = 10
+        self._is_temporal_model = is_temporal_model
+        self.load_model(model_directory, model_name)
+        super().__init__()
+
+    def load_model(self, model_directory, model_name):
+        self.name = model_name
+        self.model_full_path = model_directory + model_name
+        # Loads metadata from json file
+        json_file = open(model_directory + model_name + '.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = model_from_json(loaded_model_json)
+        # Load weights into new model
+        model.load_weights(model_directory + model_name + '.h5')
+        # Loads metadata from database
+        model.compile(loss='mse', optimizer='adam', metrics=['mse'])
+        self.model = model
+        self.reference_dataset = None
+        self.centroid = (-1, -1)
+
+    def invoke(self, X):
+        #from time import time
+        #x = time()
+        temp = self.get_model().predict(X)
+        #print("Tempo de invocação: ", time() - x)
+        return temp
+
+    def get_model(self):
+        return self.model
+
+    def get_shape(self):
+        return self.model.input.shape
+
+    def set_model(self, model):
+        self.model = model
+
+    def get_name(self):
+        return self.name
+
+    def is_temporal_model(self):
+        return self._is_temporal_model
+
+    def get_reference_dataset(self):
+        if self.reference_dataset is None:
+            dataset_full_path = self.model_directory + self.model_name + '.npy'
+            self.reference_dataset = (np.load(dataset_full_path))
+        return self.reference_dataset
+
+    def get_reference_dataset_mock(self):
+        #1. Get the learner's spatial coordinates
+        #2. Obtain the reference dataset from stream (e.g. directly from kafka)
+        # - Use data aggregator? Another Class? Where does DJEnsemble access the stream from?
+        #todo - Swap Mock
+        dataset_mock = np.full((100, 10, 10), 10.0)
+        return dataset_mock
+
+    @abstractmethod
+    def evaluate(self, reference_dataset: np.array):
+        pass
+
+    def characterize_dataset_time_series(self, target_dataset):
+        print("Calculating GLDs...")
+        time, lat, long = target_dataset.shape
+        gld_list = calculate_gld_list_from_dataset(target_dataset)
+        gld_list = np.reshape(gld_list, (lat, long, 4))  # Number of GLD parameters = 4
+        print("Calculating Centroid...")
+        return calculate_centroid(gld_list, (0, 0), (lat, long))
+
+    def compare_dataset_distances(self, original_dataset, compared_dataset, c1, c2):
+        # 2. Identify the centroid time series C1 and C2 based on the resulting parameters
+        s1 = original_dataset[:, c1[0], c1[1]]
+        s2 = compared_dataset[:, c2[0], c2[1]]
+        # 3. Determine the distance between C1 and C2 (e.g. using euclidian dist between vectors)
+        output = dtw.dtw(s1, s2)
+        return output.distance
+
+    def calculate_centroid_coordinate(self, target_dataset):
+        # Characterize each dataset time series (e.g. using GLD or Autoencoder)
+        print("Characterizing time series for dataset of shape ", target_dataset.shape)
+        return self.characterize_dataset_time_series(target_dataset)
+
+    def get_reference_dataset_centroid_coordinate(self):
+        return (0, 0)
+        #if self.centroid == (-1, -1):
+        #    self.centroid = self.calculate_centroid_coordinate(self.get_reference_dataset())
+        #return self.centroid
+
+    def update_eef(self):
+        print("Updating EEF - Model " + self.model_name)
+        reference_dataset = self.get_reference_dataset()
+        noise_dataset = reference_dataset.copy()
+
+        # Measures model performance
+        distances = []
+        error = []
+        centroid = self.get_reference_dataset_centroid_coordinate()
+        #for i in range(5):
+        for i in range(1):
+            print("Evaluating model", self.model_name, " on noise dataset ", i)
+            error.append(self.evaluate(noise_dataset[:100]))
+            distances.append(self.compare_dataset_distances(reference_dataset, noise_dataset,
+                                                                centroid, centroid))
+            NoiseGenerator().add_noise(noise_dataset)
+
+        # Fit line
+        r = LinearRegressor(distances, error)
+        r.train()
+        self.r = r
+
+    def execute_eef(self, dataset, centroid):
+        reference_dataset = self.get_reference_dataset()
+        ref_centroid = self.get_reference_dataset_centroid_coordinate()
+        x = self.compare_dataset_distances(reference_dataset, dataset, ref_centroid, centroid)
+        return self.r.predict(x)
+
+class UnidimensionalLearner(Learner):
+    def evaluate(self, target_dataset: np.array):
+        time, lat, long = target_dataset.shape
+        rmse_vector = []
+        for i in range(lat):
+            for j in range(long):
+                cut_start = 0
+                cut_ending = self.temporal_length + self.number_of_training_samples
+
+                X, y = SeriesGenerator().split_series_into_set_of_fixed_size_series(
+                    target_dataset[cut_start:cut_ending, i, j], self.temporal_length, 1)
+                # X = X.reshape((self.number_of_samples, self.temporal_length, 1))
+                output = self.invoke(X)
+                region_rmse = tf.sqrt(tf.math.reduce_mean(tf.losses.mean_squared_error(output, y)))
+                rmse_vector.append(region_rmse.numpy())
+        average_rmse = reduce(lambda a, b: a + b, rmse_vector) / len(rmse_vector)
+        return average_rmse
+
+class MultidimensionalLearner(Learner):
+    def evaluate(self, target_dataset: np.array):
+        invoker = ConvolutionalModelInvoker()
+        return invoker.evaluate_convolutional_model(target_dataset, self)
+
+
+def test_unidimensional_layer():
+    directory = "/home/anderson/Dropbox/Doutorado/Tese/Fermatta/DJEnsemble/models/rain/temporal/"
+    name = "best_model_C4"
+    l = UnidimensionalLearner(directory, name, is_temporal_model=True)
+    l.update_eef()
+    print(l.execute_eef(NoiseGenerator().add_noise(l.get_reference_dataset())))
+
+def test_multidimensional_layer():
+    directory = "/home/anderson/Dropbox/Doutorado/Tese/Fermatta/DJEnsemble/models/rain/convolutional/"
+    name = "best_DJ1_v1_upd"
+    l = MultidimensionalLearner(directory, name, is_temporal_model=False)
+    l.update_eef()
+    print(l.execute_eef(NoiseGenerator().add_noise(l.get_reference_dataset())))
+
+if __name__ == "__main__":
+    test_unidimensional_layer()
+    test_multidimensional_layer()
